18e31dec3601c4d8cfbed9bb633917627d53b12f
Sabale302 <71699671+Sabale302@users.noreply.github.com>
2025-04-19 10:08:29 +0530

    Commit

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,10 +1,12 @@
-import { Sequelize } from "sequelize";
-import process from 'process';
-import { config } from 'dotenv';
 import fs from 'fs';
 import path from 'path';
-
+import { Sequelize } from 'sequelize';
+import process from 'process';
+import { config } from 'dotenv';
 config();
+
+// Directly use path to ca.pem assuming it's in the 'certs' folder in your project root
+const caCertPath = path.resolve('./ca.pem');
 
 const sequelize = new Sequelize(
   process.env.MYSQL_DB,
@@ -12,15 +14,17 @@ const sequelize = new Sequelize(
   process.env.MYSQL_PASSWORD,
   {
     host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
-    dialect: "mysql",
+    port: process.env.MYSQL_PORT, // Don't forget this if you're using a custom port like 25891
+    dialect: 'mysql',
     dialectOptions: {
       ssl: {
-        ca: fs.readFileSync(path.join(path.resolve(), process.env.SSL_CA_PATH))
-      },
-      connectTimeout: 10000
+        ca: fs.readFileSync(caCertPath)
+      }
     },
-    logging: console.log // optional for debugging
+    pool: {
+      acquire: 30000,
+      idle: 10000,
+    }
   }
 );
 
