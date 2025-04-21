6fc95ccc7fe77d3cedb132af5fc78f9c9a8cfa89
Sabale302 <71699671+Sabale302@users.noreply.github.com>
2025-04-21 15:18:54 +0530

    Home UI updated

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -122,7 +122,7 @@ const Home = () => {
                   sx={{
                     p: 2,
                     borderRadius: 2,
-                    width: 200,
+                    width: 215,
                     textAlign: "center",
                     backgroundColor: stat.cardBg,
                     transition: "0.3s",
